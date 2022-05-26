905b74032c48ed280514e96201b1a3a84f336fb6
Emanuel Arroyo <emanuelarroyodev@gmail.com>
2022-05-26 20:41:29 -0300

    deploy exercise basic

---
+++ script.js
@@ -0,0 +1,20 @@
+const category = document.querySelector("#categoria");
+const amount = document.querySelector("#cantidad");
+const btnResume = document.querySelector("#btnResumen");
+const btnReset = document.querySelector("#btnReset");
+const output = document.querySelector("#output");
+
+btnResume.onclick = () => {
+  output.innerHTML = "Total a Pagar: $ ";
+  let discountSelected = +category.options[+category.selectedIndex].value;
+  let amountTickets = +amount.value;
+  const priceTicket = 200;
+  const priceNotDiscount = priceTicket * amountTickets;
+  const priceWithDiscount =
+    priceNotDiscount - (priceNotDiscount * discountSelected) / 100;
+  output.innerHTML += priceWithDiscount;
+};
+
+btnReset.onclick = () => {
+  output.innerHTML = "Total a Pagar: $ ";
+};
